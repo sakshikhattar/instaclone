f96e9f582f0cef26fd3f6e67612556171ea9e4bf
sakshikhattar <sakshikhattar1@gmail.com>
2017-07-28 22:59:50 +0530

    classification of dirty images

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -3,29 +3,33 @@ from models import UserModel, PostModel, LikeModel, CommentModel
 
 
 class SignUpForm(forms.ModelForm):
-  class Meta:
-    model = UserModel
-    fields = ['email', 'username', 'name', 'password']
+    class Meta:
+        model = UserModel
+        fields=['email','username','name','password']
 
 class LoginForm(forms.ModelForm):
-  class Meta:
-    model = UserModel
-    fields = ['username', 'password']
+    class Meta:
+        model = UserModel
+        fields = ['username', 'password']
 
 class PostForm(forms.ModelForm):
-  class Meta:
-    model = PostModel
-    fields = ['image', 'caption']
+    class Meta:
+        model = PostModel
+        fields=['image', 'caption']
+
 
 class LikeForm(forms.ModelForm):
-  class Meta:
-    model = LikeModel
-    fields = ['post']
+
+    class Meta:
+        model = LikeModel
+        fields=['post']
+
 
 class CommentForm(forms.ModelForm):
-  class Meta:
-    model = CommentModel
-    fields = ['comment_text', 'post']
+
+    class Meta:
+        model = CommentModel
+        fields = ['comment_text', 'post']
 
 
 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -5,14 +5,19 @@ from django.shortcuts import render, redirect
 from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
 from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
 from django.contrib.auth.hashers import make_password, check_password
-from datetime import datetime
-from imgurpython import ImgurClient
+from datetime import timedelta
+from django.utils import timezone
 from mysite.settings import BASE_DIR
+import requests
+from clarifai.rest import ClarifaiApp, Image as CImage
+from imgurpython import ImgurClient
+from enum import Enum
+import sendgrid
+
 
 # Create your views here.
 
 def signup_view(request):
-    today = datetime.now()
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -20,17 +25,19 @@ def signup_view(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            # saving data to DB
             user = UserModel(name=name, password=make_password(password), email=email, username=username)
             user.save()
             return render(request, 'success.html')
-
-    elif request.method == 'GET':
+            # return redirect('login/')
+    else:
         form = SignUpForm()
 
-    return render(request, 'index.html', {'today': today}, {'form': form})
+    return render(request, 'index.html', {'form': form})
 
 
 def login_view(request):
+    response_data = {}
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -46,15 +53,65 @@ def login_view(request):
                     response = redirect('feed/')
                     response.set_cookie(key='session_token', value=token.session_token)
                     return response
-
-
                 else:
-                    print 'User is invalid'
+                    response_data['message'] = 'Incorrect Password! Please try again!'
 
-    elif request.method == "GET":
+    elif request.method == 'GET':
         form = LoginForm()
 
-    return render(request, 'login.html', {'form': form})
+    response_data['form'] = form
+    return render(request, 'login.html', response_data)
+
+
+def post_view(request):
+    user = check_validation(request)
+
+    if user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data.get('image')
+                caption = form.cleaned_data.get('caption')
+                post = PostModel(user=user, image=image, caption=caption)
+                post.save()
+                apikey = 'lBzto9IhYQnI8Z6kd4dFap0gGbFexBgRBknxuISGFK4'
+                request_url = ('https://apis.paralleldots.com/sentiment?sentence1=%s&apikey=%s') % (caption, apikey)
+                print 'POST request url : %s' % (request_url)
+                sentiment = requests.get(request_url, verify=False).json()
+                sentiment_value = sentiment['sentiment']
+
+                path = str(BASE_DIR + '\\' + post.image.url)
+
+                client = ImgurClient("315c0833408f9c0", "ab94bfdc68d430ac6f7aa5f16260b1f5d6e27b5e")
+                post.image_url = client.upload_from_path(path, anon=True)['link']
+                print post.image_url
+                post.save()
+                keywords = ['garbage', 'waste', 'trash', 'dirt', 'pollution', 'dust']
+                value_list = []
+                app = ClarifaiApp(api_key='de3548011bc94054bf8f2cc57f84376e')
+
+                model = app.models.get('general-v1.3')
+                image = CImage(url=post.image_url)
+                prediction = model.predict([image])
+
+                for i in range(0, len(prediction['outputs'][0]['data']['concepts'])):
+                    if prediction['outputs'][0]['data']['concepts'][i]['name'] in keywords:
+                        value = prediction['outputs'][0]['data']['concepts'][i]['value']
+                        value_list.append(value)
+
+                if (sentiment_value < 0.6 and max(value_list) > 0.8):
+                    print 'dirty image'
+
+                return redirect('/feed/')
+
+        else:
+            form = PostForm()
+        return render(request, 'post.html', {'form': form})
+    else:
+        return redirect('/login/')
+
+
+
 
 
 def feed_view(request):
@@ -74,38 +131,6 @@ def feed_view(request):
         return redirect('/login/')
 
 
-# For validating the session
-def check_validation(request):
-    if request.COOKIES.get('session_token'):
-        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
-        if session:
-            return session.user
-    else:
-        return None
-
-
-def post_view(request):
-    user = check_validation(request)
-
-    if user:
-        if request.method == 'GET':
-            form = PostForm()
-            return render(request, 'post.html', {'form': form})
-        elif request.method == 'POST':
-            form = PostForm(request.POST, request.FILES)
-            if form.is_valid():
-                image = form.cleaned_data.get('image')
-                caption = form.cleaned_data.get('caption')
-                post = PostModel(user=user, image=image, caption=caption)
-                path = str(BASE_DIR + '\\' + post.image.url)
-                client = ImgurClient("315c0833408f9c0", "ab94bfdc68d430ac6f7aa5f16260b1f5d6e27b5e")
-                post.image_url = client.upload_from_path(path, anon=True)['link']
-                post.save()
-                return redirect('/feed/')
-    else:
-        return redirect('/login/')
-
-
 def like_view(request):
     user = check_validation(request)
     if user and request.method == 'POST':
@@ -120,6 +145,7 @@ def like_view(request):
             return redirect('/feed/')
     else:
         return redirect('/login/')
+
 
 def comment_view(request):
     user = check_validation(request)
@@ -137,3 +163,13 @@ def comment_view(request):
         return redirect('/login')
 
 
+# For validating the session
+def check_validation(request):
+    if request.COOKIES.get('session_token'):
+        session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
+        if session:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
+    else:
+        return None
